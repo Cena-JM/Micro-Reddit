fce4eb2a6145f7fd82ffefdc1b9a6d14d74736e4
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-04-16 17:13:01 +0300

    Add user model and validation

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 6, maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+end
