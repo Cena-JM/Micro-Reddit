bb3b33955aad4bacf7f768b5ed5974e0fdc2384a
kvnlay <kingstevens007@gmail.com>
2019-04-16 18:45:40 +0100

    Added comment model and added post and user ref

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+  has_many :posts
+  has_many :comments
 end
