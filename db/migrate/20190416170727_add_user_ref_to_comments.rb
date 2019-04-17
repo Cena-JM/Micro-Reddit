5df2e639e5fe1fda44a5333483d50204eb9890d1
kvnlay <kingstevens007@gmail.com>
2019-04-17 12:48:07 +0100

    Linter error fixes (4)

--- db/migrate/20190416170727_add_user_ref_to_comments.rb
+++ db/migrate/20190416170727_add_user_ref_to_comments.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#AddUserRefToComments
+# AddUserRefToComments
 class AddUserRefToComments < ActiveRecord::Migration[5.2]
   def change
     add_reference :comments, :user, foreign_key: true
